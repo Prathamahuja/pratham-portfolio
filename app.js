651eaf0e8ef0409e3c653034b6d07d25e9873a2b
Pratham Ahuja <prathamahuja463@gmail.com>
2024-07-11 23:07:58 +0530

    commited

---
+++ app.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["React.js developer" , "Full stack Web developer", "react Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
